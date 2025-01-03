4d6f6b8f4a77053aa4878d1db5027b1997952abb
DanishExpertizo <muhammad.danish@expertizo.pk>
2025-01-03 16:24:40 +0500

    Update QuizView.tsx

--- src/views/QuizView.tsx
+++ src/views/QuizView.tsx
@@ -1,13 +1,23 @@
+'use client'
 import { QuizDataInterface } from "@/interfaces/Types";
-import QuizContent from "@/components/QuizContent";
-import { getQuizData } from "../lib/getData";
-import Loader from "@/components/loader/Loader";
+import QuizContent from "../components/QuizContent";
+import jsonData from '../data/questions.json';
+import shuffle from "../utils/Shuffle";
 
-export default async function QuizView() {
-    const questions = await getQuizData();
+const QuizView = () => {
 
-    if (!questions || questions.length === 0) return <Loader />;
+    const decodedData = jsonData.map((question: QuizDataInterface) => ({
+        ...question,
+        category: decodeURIComponent(question.category),
+        correct_answer: decodeURIComponent(question.correct_answer),
+        incorrect_answers: question.incorrect_answers.map((answer: string) =>
+            decodeURIComponent(answer)
+        ),
+        question: decodeURIComponent(question.question),
+    }));
+    const shuffledData = shuffle(decodedData);
 
-    return <QuizContent shuffledData={questions as QuizDataInterface[]} />;
+    return (<QuizContent shuffledData={shuffledData as QuizDataInterface[]} />)
 }
 
+export default QuizView;
